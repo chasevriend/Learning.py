2d15f93894d8a2fdd5dfbd191ef5918e2b220b46
Chase <chasevriend84@gmail.com>
2023-01-20 06:26:15 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -117,9 +117,19 @@
 # print(people)
 
 #Exceptions
-x = int(input("x: "))
-y = int(input("y: "))
+import sys
 
-result = x / y
+try:
+    x = int(input("x: "))
+    y = int(input("y: "))
+except ValueError:
+    print("Error invalid input")
+    sys.exit(1)
+
+try:
+    result = x / y
+except ZeroDivisionError:
+    print("Error: Cannot divide by 0")
+    sys.exit(1)
 
 print(f"{x} / {y} = {result}")
